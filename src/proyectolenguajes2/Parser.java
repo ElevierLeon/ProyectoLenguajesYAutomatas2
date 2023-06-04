c08303472372fe0315aaf9b43e11e991063ea4b7
ElevierLeon <18170367@itculiacan.edu.mx>
2023-06-04 00:05:50 -0700

    Se agregaron cambios al Parser y token

--- src/proyectolenguajes2/Parser.java
+++ src/proyectolenguajes2/Parser.java
@@ -6,11 +6,12 @@
 package proyectolenguajes2;
 
 import java.util.ArrayList;
+
 public class Parser {
-    private final ArrayList<Token> tokens;  // Lista de tokens de entrada
-    private int currentTokenIndex;  // Índice del token actual
-    private TreeNode root;  // Nodo raíz del árbol de análisis sintáctico
-    private boolean syntaxError;  // Variable que indica si se ha encontrado un error de sintaxis
+    private final ArrayList<Token> tokens;
+    private int currentTokenIndex;
+    private TreeNode root;
+    private boolean syntaxError;
 
     public Parser(ArrayList<Token> tokens) {
         this.tokens = tokens;
@@ -30,7 +31,7 @@ public class Parser {
                 int errorTokenIndex = currentTokenIndex - 1;
                 if (errorTokenIndex >= 0 && errorTokenIndex < tokens.size()) {
                     Token errorToken = tokens.get(errorTokenIndex);
-                    //System.out.println("Error at token: " + errorToken);
+                    System.out.println("Error at token: " + errorToken);
                 }
             }
             return null;
@@ -38,25 +39,20 @@ public class Parser {
     }
 
     private TreeNode program() {
-        // El método program() representa la producción "program -> declarationList"
         TreeNode declarationListNode = declarationList();
         if (declarationListNode != null) {
-            // Se crea un nuevo nodo con etiqueta "program" y se agrega el nodo de declaración
             return new TreeNode("program", declarationListNode);
         }
         return null;
     }
 
     private TreeNode declarationList() {
-        // El método declarationList() representa la producción "declarationList -> declaration declarationList"
         TreeNode declarationNode = declaration();
         if (declarationNode != null) {
             TreeNode declarationListNode = declarationList();
             if (declarationListNode != null) {
-                // Se crea un nuevo nodo con etiqueta "declaration_list" y se agregan los nodos de declaración y de lista de declaración
                 return new TreeNode("declaration_list", declarationNode, declarationListNode);
             } else {
-                // Solo hay un nodo de declaración, no hay más nodos de lista de declaración
                 return new TreeNode("declaration_list", declarationNode);
             }
         }
@@ -64,12 +60,10 @@ public class Parser {
     }
 
     private TreeNode declaration() {
-        // El método declaration() representa la producción "declaration -> typeSpecifier identifier"
         TreeNode typeSpecifierNode = typeSpecifier();
         if (typeSpecifierNode != null) {
             TreeNode identifierNode = identifier();
-            if (identifierNode != null && match(Token.TYPE_SPECIFIER)) {
-                // Se crea un nuevo nodo con etiqueta "declaration" y se agregan los nodos de typeSpecifier e identifier
+            if (identifierNode != null && match(Token.SEMICOLON)) {
                 return new TreeNode("declaration", typeSpecifierNode, identifierNode);
             } else {
                 syntaxError = true;
@@ -81,25 +75,144 @@ public class Parser {
     }
 
     private TreeNode typeSpecifier() {
-        // El método typeSpecifier() representa la producción "typeSpecifier -> TYPE_SPECIFIER"
-        if (match(Token.TYPE_SPECIFIER)) {
-            // Se crea un nuevo nodo con etiqueta "type_specifier" y el valor del token actual
+        if (match(Token.INT) || match(Token.FLOAT) || match(Token.CHAR) || match(Token.DOUBLE)) {
             return new TreeNode("type_specifier", previousToken().getTokenValue());
         }
         return null;
     }
 
     private TreeNode identifier() {
-        // El método identifier() representa la producción "identifier -> IDENTIFIER"
         if (match(Token.IDENTIFIER)) {
-            // Se crea un nuevo nodo con etiqueta "identifier" y el valor del token actual
             return new TreeNode("identifier", previousToken().getTokenValue());
         }
         return null;
     }
 
+    private TreeNode statement() {
+        if (match(Token.IF)) {
+            if (match(Token.OPEN_PAREN)) {
+                TreeNode conditionNode = condition();
+                if (conditionNode != null && match(Token.CLOSE_PAREN) && match(Token.OPEN_BRACE)) {
+                    TreeNode trueStatementListNode = statementList();
+                    if (match(Token.CLOSE_BRACE) && match(Token.ELSE) && match(Token.OPEN_BRACE)) {
+                        TreeNode falseStatementListNode = statementList();
+                        if (match(Token.CLOSE_BRACE)) {
+                            return new TreeNode("if_statement", conditionNode, trueStatementListNode, falseStatementListNode);
+                        }
+                    }
+                }
+            }
+        } else if (match(Token.WHILE)) {
+            if (match(Token.OPEN_PAREN)) {
+                TreeNode conditionNode = condition();
+                if (conditionNode != null && match(Token.CLOSE_PAREN) && match(Token.OPEN_BRACE)) {
+                    TreeNode statementListNode = statementList();
+                    if (match(Token.CLOSE_BRACE)) {
+                        return new TreeNode("while_statement", conditionNode, statementListNode);
+                    }
+                }
+            }
+        } else {
+            return assignmentStatement();
+        }
+        return null;
+    }
+
+    private TreeNode assignmentStatement() {
+        TreeNode identifierNode = identifier();
+        if (identifierNode != null && match(Token.ASSIGNMENT)) {
+            TreeNode expressionNode = expression();
+            if (expressionNode != null && match(Token.SEMICOLON)) {
+                return new TreeNode("assignment_statement", identifierNode, expressionNode);
+            } else {
+                syntaxError = true;
+            }
+        } else {
+            syntaxError = true;
+        }
+        return null;
+    }
+
+    private TreeNode condition() {
+        TreeNode expression1Node = expression();
+        if (expression1Node != null) {
+            TreeNode operatorNode = relationalOperator();
+            if (operatorNode != null) {
+                TreeNode expression2Node = expression();
+                if (expression2Node != null) {
+                    return new TreeNode("condition", expression1Node, operatorNode, expression2Node);
+                }
+            }
+        }
+        return null;
+    }
+
+    private TreeNode relationalOperator() {
+        if (match(Token.LESS_THAN) || match(Token.GREATER_THAN) || match(Token.EQUALS) ||
+                match(Token.NOT_EQUALS) || match(Token.LESS_THAN_OR_EQUAL) || match(Token.GREATER_THAN_OR_EQUAL)) {
+            return new TreeNode("relational_operator", previousToken().getTokenValue());
+        }
+        return null;
+    }
+
+    private TreeNode expression() {
+        TreeNode termNode = term();
+        if (termNode != null) {
+            TreeNode expressionNode = expression();
+            if (expressionNode != null) {
+                return new TreeNode("expression", expressionNode, termNode);
+            } else {
+                return new TreeNode("expression", termNode);
+            }
+        }
+        return null;
+    }
+
+    private TreeNode term() {
+        TreeNode factorNode = factor();
+        if (factorNode != null) {
+            TreeNode termNode = term();
+            if (termNode != null) {
+                return new TreeNode("term", termNode, factorNode);
+            } else {
+                return new TreeNode("term", factorNode);
+            }
+        }
+        return null;
+    }
+
+    private TreeNode factor() {
+        if (match(Token.IDENTIFIER)) {
+            return new TreeNode("identifier", previousToken().getTokenValue());
+        } else if (match(Token.CONSTANT)) {
+            return new TreeNode("constant", previousToken().getTokenValue());
+        } else if (match(Token.OPEN_PAREN)) {
+            TreeNode expressionNode = expression();
+            if (expressionNode != null && match(Token.CLOSE_PAREN)) {
+                return new TreeNode("factor", expressionNode);
+            } else {
+                syntaxError = true;
+            }
+        } else {
+            syntaxError = true;
+        }
+        return null;
+    }
+
+    private TreeNode statementList() {
+        TreeNode statementNode = statement();
+        if (statementNode != null) {
+            TreeNode statementListNode = statementList();
+            if (statementListNode != null) {
+                return new TreeNode("statement_list", statementNode, statementListNode);
+            } else {
+                return new TreeNode("statement_list", statementNode);
+            }
+        }
+        return null;
+    }
+
     private boolean match(String expectedType) {
-        // El método match() verifica si el tipo del token actual coincide con el tipo esperado
         if (currentTokenIndex < tokens.size()) {
             Token currentToken = tokens.get(currentTokenIndex);
             if (currentToken.getTokenType().equals(expectedType)) {
@@ -111,7 +224,6 @@ public class Parser {
     }
 
     private Token previousToken() {
-        // El método previousToken() obtiene el token anterior al token actual
         if (currentTokenIndex > 0) {
             return tokens.get(currentTokenIndex - 1);
         }
--- src/proyectolenguajes2/Token.java
+++ src/proyectolenguajes2/Token.java
@@ -17,23 +17,51 @@ public class Token {
     public static final String CONSTANT = "Constant";
     public static final String MAIN_FUNCTION = "Main Function";
     public static final String UNKNOWN = "Unknown";
+    public static final String PUNCTUATION = "Punctuation";
+
+    // Tipos de token específicos para los tipos de datos
+    public static final String INT = "int";
+    public static final String FLOAT = "float";
+    public static final String CHAR = "char";
+    public static final String DOUBLE = "double";
     
+    // Palabras clave
+    public static final String IF = "if";
+    public static final String WHILE = "while";
+    public static final String ELSE = "else";
+    
+    // Símbolos de puntuación
+    public static final String OPEN_PAREN = "(";
+    public static final String CLOSE_PAREN = ")";
+    public static final String OPEN_BRACE = "{";
+    public static final String CLOSE_BRACE = "}";
+    public static final String SEMICOLON = ";";
+    
+    // Operadores
+    public static final String ASSIGNMENT = "=";
+    public static final String GREATER_THAN = ">";
+    public static final String EQUALS = "==";
+    public static final String LESS_THAN = "<";
+    public static final String GREATER_THAN_OR_EQUAL = ">=";
+    public static final String NOT_EQUALS = "!=";
+    public static final String LESS_THAN_OR_EQUAL = "<=";
+
     private String tokenType;
     private String tokenValue;
-    
+
     public Token(String tokenType, String tokenValue) {
         this.tokenType = tokenType;
         this.tokenValue = tokenValue;
     }
-    
+
     public String getTokenType() {
         return tokenType;
     }
-    
+
     public String getTokenValue() {
         return tokenValue;
     }
-    
+
     @Override
     public String toString() {
         return "Token [Type: " + tokenType + ", Value: " + tokenValue + "]";
